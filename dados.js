247d33c0360782e78ed20f92a07352425c8f6917
aryapromotora-alt <eduardomurtada@gmail.com>
2026-09-17 17:12:44 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,17 @@
+const bancos = [
+    { id: 'banrisul', nome: 'BANRISUL', data: '17/09/2026' },
+    { id: 'bmg', nome: 'BMG', data: '17/09/2026' },
+    { id: 'brb', nome: 'BRB RED Consig', data: '17/09/2026' },
+    { id: 'digio', nome: 'DIGIO', data: '17/09/2026' },
+    { id: 'facta', nome: 'FACTA', data: '17/09/2026' },
+    { id: 'safra', nome: 'SAFRA', data: '17/09/2026' },
+    { id: 'daycoval', nome: 'DAYCOVAL', data: '17/09/2026' },
+    { id: 'quero-mais', nome: 'QUERO MAIS', data: '17/09/2026' },
+    { id: 'c6-bank', nome: 'C6 BANK', data: '17/09/2026' },
+    { id: 'finanto', nome: 'FINANTO', data: '17/09/2026' },
+    { id: 'icred', nome: 'ICRED', data: '17/09/2026' },
+    { id: 'pan', nome: 'PAN', data: '17/09/2026' },
+    { id: 'parana-banco', nome: 'PARANÁ BANCO', data: '17/09/2026' },
+    { id: 'quali', nome: 'QUALI', data: '17/09/2026' }
+    { id: 'total-cash', nome: 'QUALI', data: '17/09/2026' }  
+];
